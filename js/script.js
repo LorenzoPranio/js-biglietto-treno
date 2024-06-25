1d9cf5c41f22dc8fe82fdde05743fcf4e10c2b5d
LorenzoPranio <lorenzopranio02@gmail.com>
2024-06-25 15:30:27 +0200

    [UPD]Added age and kilometri prompt in script.js

---
+++ js/script.js
@@ -0,0 +1,11 @@
+//KM da percorrere
+let kilometri = prompt('Inserisci i kilometri che devi percorrere');
+//Età 
+let age = prompt('Inserisci la tua età');
+//Prezzo biglietto
+
+//Sconto
+
+//Prezzo finale
+
+//if
