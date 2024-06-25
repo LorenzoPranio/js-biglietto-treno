8b1078d723e1a73b482733755e32d6cb2e3f3b11
LorenzoPranio <lorenzopranio02@gmail.com>
2024-06-25 15:34:43 +0200

    [UPD]Added prezzo bigleitto, sconto and prezzo finale in script.js

--- js/script.js
+++ js/script.js
@@ -3,9 +3,9 @@ let kilometri = prompt('Inserisci i kilometri che devi percorrere');
 //Età 
 let age = prompt('Inserisci la tua età');
 //Prezzo biglietto
-
+let prezzo_biglietto = 0.21 * kilometri;
 //Sconto
-
+let sconto = 0;
 //Prezzo finale
-
+let prezzo_finale = 0;
 //if
